06f3844805c09a757e86c0ad95fe55eb4c43f2a1
Nikuzo <41453940+NikuzoKO@users.noreply.github.com>
2024-07-16 18:38:22 -0300

    Product controller

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,36 +1,41 @@
 const { createClient } = require("@supabase/supabase-js");
 const { default: slugify } = require("slugify");
-const { Product, Type } = require("../models");
 const formidable = require("formidable");
 const path = require("path");
 const fs = require("fs");
 
+const { Product, Type } = require("../models");
+
 const ProductController = {
     showList: async (req, res) => {
         try {
+            const options = { include: Type };
             const limit = Number(req.query.limit);
             const typeId = Number(req.query.typeId);
-            /*             const offset = req.query.page ? (Number(req.query.page) - 1) * limit : 0;
-            const products = await Product.findAll({ offset: offset, limit: limit, include: Type }); */
-            const options = { include: Type };
+            const offset = req.query.page ? (Number(req.query.page) - 1) * limit : 0;
+
+            if (offset) options.offset = offset;
             if (limit) options.limit = limit;
             if (typeId) {
                 if (await Type.findByPk(typeId)) options.where = { typeId };
             }
+
             const products = await Product.findAll(options);
+
             return res.json({ products });
         } catch (err) {
             console.error(err);
-            return res.send({ msg: "Failed to show products" });
+            return res.status(400).send({ msg: "Failed to show products" });
         }
     },
     show: async (req, res) => {
         try {
             const product = await Product.findByPk(req.params.id, { include: Type });
+
             return res.json({ product });
         } catch (err) {
             console.error(err);
-            return res.send({ msg: "Failed to show product" });
+            return res.status(400).send({ msg: "Failed to show product" });
         }
     },
     store: async (req, res) => {
@@ -82,9 +87,7 @@ const ProductController = {
         try {
             const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
-            const form = formidable({
-                keepExtensions: true,
-            });
+            const form = formidable({ keepExtensions: true });
 
             form.parse(req, async (err, fields, files) => {
                 try {
@@ -94,9 +97,12 @@ const ProductController = {
 
                     const product = await Product.findByPk(req.params.id);
 
-                    const { data, error } = await supabase.storage
+                    const removeImage = await supabase.storage
                         .from("images")
-                        .remove([`products/${product.image}`])
+                        .remove([`products/${product.image}`]);
+
+                    const uploadImage = await supabase.storage
+                        .from("images")
                         .upload(`products/${image}`, fs.createReadStream(files.image.filepath), {
                             cacheControl: "3600",
                             upsert: false,
@@ -127,11 +133,19 @@ const ProductController = {
     },
     destroy: async (req, res) => {
         try {
-            await Product.destroy({ where: { id: req.params.id } });
+            const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+            const product = await Product.findByPk(req.params.id);
+
+            const { data, error } = await supabase.storage
+                .from("images")
+                .remove([`products/${product.image}`]);
+
+            await product.destroy();
             return res.send({ msg: "Product successfully deleted" });
         } catch (err) {
             console.error(err);
-            return res.send({ msg: "Failed to delete product" });
+            return res.status(400).send({ msg: "Failed to delete product" });
         }
     },
 };
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,8 @@
+const { expressjwt: checkJwt } = require("express-jwt");
 const express = require("express");
 const router = express.Router();
+
 const ProductController = require("../controllers/ProductController");
-const { expressjwt: checkJwt } = require("express-jwt");
 const permissionRequired = require("../middlewares/permissionRequired");
 
 router.get("/", ProductController.showList);
@@ -13,6 +14,7 @@ router.use(
         algorithms: ["HS256"],
     }),
 );
+
 router.post("/create", permissionRequired.admin, ProductController.store);
 router.post("/edit/:id", permissionRequired.admin, ProductController.update);
 router.get("/delete/:id", permissionRequired.admin, ProductController.destroy);
